da91e5062f5bb9dc017b364bf170f82df3b41c98
pruetsachap-ux <pruetsacha.p@gmail.com>
2025-12-14 10:45:38 +0700

    fix: clean up Header component code and improve scroll event handling

--- src/shared/components/layouts/header/Header.jsx
+++ src/shared/components/layouts/header/Header.jsx
@@ -1,26 +1,34 @@
-"use client"
-import { cn } from '@/shared/lib/utils'
-import React, { useEffect, useState } from 'react'
+"use client";
+import { cn } from "@/shared/lib/utils";
+import { useEffect, useState } from "react";
 
 export default function Header() {
-    const [isScrolled, setIsScrolled] = useState(false)
+  const [isScrolled, setIsScrolled] = useState(false);
+  console.log(isScrolled);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if(window.scrollY > 0 ) {
-                setIsScrolled(true)
-            } else {
-                setIsScrolled(false)
-            }
+      window.addEventListener("scroll", handleScroll);
 
-            window.addEventListener("scroll", handleScroll)
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    };
+  }, []);
 
-            return() => {
-                window.removeEventListener("scroll", handleScroll)
-            }
-        }
-    }, [])
   return (
-    <header className={cn("fixed top-0 left-0 z-50 p-4", isScrolled ? "bg-white": "bg-amber-400")}>Header</header>
-  )
+    <header
+      className={cn(
+        "fixed top-0 left-0 z-50 p-4",
+        isScrolled ? "bg-white" : ""
+      )}
+    >
+      Header
+    </header>
+  );
 }
